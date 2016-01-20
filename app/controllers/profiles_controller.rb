542cb11d77145ce7ee6c574945909a5c34c98440
Johnny Hu <jgeehen.hu@gmail.com>
2016-01-20 00:30:10 +0000

    Add before filters to user and profile controllers to check user authentication

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,7 @@
 class ProfilesController < ApplicationController
+  before_action :authenticate_user! #Devise gem feature to verify that the user has the authorization to access user profiles
+  before_action :only_current_user #defined below
+  
   def new
     # form where a user can fill out their own profile.
     @user = User.find( params[:user_id] )
@@ -21,9 +24,26 @@ class ProfilesController < ApplicationController
     @profile = @user.profile
   end
   
+  def update
+    @user = User.find( params[:user_id] ) # grabs user_id from the url form, as set up using Devise's current_user in the pages' new.html.erb page
+    @profile = @user.profile
+    if @profile.update_attributes(profile_params)
+      flash[:success] = "Profile updated!"
+      redirect_to user_path( params[:user_id] )
+    else
+      flash[:warning] = "Something went wrong! Please try again."
+      render action: :edit
+    end
+  end
+  
   private
   
     def profile_params 
       params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)  
+    end
+    
+    def only_current_user
+      @user = User.find( params[:user_id] )
+      redirect_to(root_url) unless @user == current_user
     end
 end
